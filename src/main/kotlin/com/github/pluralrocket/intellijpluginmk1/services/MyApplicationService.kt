2c13c763d7bfa00a78a87495e58923cf28328aa0
GitHub Action <action@github.com>
2022-04-25 11:42:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pluralrocket/intellijpluginmk1/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pluralrocket.intellijpluginmk1.services
+
+import com.github.pluralrocket.intellijpluginmk1.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pluralrocket/intellijpluginmk1/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pluralrocket/intellijpluginmk1/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pluralrocket.intellijpluginmk1.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pluralrocket.intellijpluginmk1.MyBundle
 
 class MyProjectService(project: Project) {
 
